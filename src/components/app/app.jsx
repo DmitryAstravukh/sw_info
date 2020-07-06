3ce98b28f8b6cb4b4108cadb4caac53f26c3a2e2
DmitryAstravukh <astrauh1.dima1@ya.ru>
2020-07-06 11:08:58 +0300

    all row

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -38,26 +38,30 @@ export default class App extends React.Component{
             <Record field="birthYear" label="Birth Year"/>
             <Record field="eyeColor" label="Eye Color"/> 
           </Row>
-{/* 
+
           <Row getData={this.swapi.getAllPlanets}
                 getItemData={this.swapi.getPlanet}
                 getImgUrl={this.swapi.getPlanetImg}
-                renderItems={item => `${item.id}. ${item.name}`}
-                fields={
-                  [
-                    {field: 'name', label: 'Name' }
-                  ]
-                }/>
+                renderItems={item => `${item.id}. ${item.name}`}>
+            
+            <Record field="population" label="Population"/>
+            <Record field="rotationPeriod" label="Rotation Period"/>
+            <Record field="diameter" label="Diameter"/> 
+          </Row>
 
           <Row getData={this.swapi.getAllStarships}
                 getItemData={this.swapi.getStarship}
                 getImgUrl={this.swapi.getStarshipImg}
-                renderItems={item => `${item.id}. ${item.name}`}
-                fields={
-                  [
-                    {field: 'name', label: 'Name' }
-                  ]
-                }/> */}
+                renderItems={item => `${item.id}. ${item.name}`}>
+            
+            <Record field="model" label="Model"/>
+            <Record field="manufacturer" label="Manufacturer"/>
+            <Record field="costInCredits" label="Cost In Credits"/> 
+            <Record field="length" label="Length"/> 
+            <Record field="crew" label="Crew"/> 
+            <Record field="passengers" label="Passengers"/> 
+            <Record field="cargoCapacity" label="Cargo Capacity"/> 
+          </Row>
 
         </div>
       </React.Fragment>
